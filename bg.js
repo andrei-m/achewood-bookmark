f9351e1b0d64118083b3bd0447ea140988931a7a
Andrei Mackenzie <andrei@clypd.com>
2016-05-22 17:32:50 -0400

    Use localstorage for URL persistance.

--- bg.js
+++ bg.js
@@ -1,15 +1,31 @@
+console.log('Achewood bookmarker loaded.');
+
 var ab = new AchewoodBookmarker();
 
 chrome.webNavigation['onCompleted'].addListener(function(data) {
   if (ab.isAchewoodURL(data.url)) {
-    console.log(data.url);
+    ab.setURL(data.url);
   }
 });
-console.log('Achewood bookmarker loaded');
+
+ab.getURL(function(data) {
+  console.log('Achewood URL: ' + data.url);
+});
 
 function AchewoodBookmarker() {
   this.achewoodURL = 'http://www.achewood.com/index.php?date=';
+
   this.isAchewoodURL = function(url) {
     return url.substring(0, this.achewoodURL.length) === this.achewoodURL
+  };
+
+  this.getURL = function(cb) {
+    chrome.storage.local.get('url', cb);
+  };
+
+  this.setURL = function(url) {
+    chrome.storage.local.set({'url': url}, function() {
+      console.log(url + ' bookmarked.');
+    });
   };
 }
